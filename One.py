cfd9a55464b07ff7e6d4e7ed6ba0efd33b9e8ad4
ChawYadiZin <chawyatizin2002@gmail.com>
2020-02-29 12:13:00 +0630

    fibonacci.py"Loop.py

---
+++ One.py
@@ -0,0 +1,112 @@
+import pygame
+pygame.int()
+
+
+win = pygame.display.set_mode((800,480))
+pygame.display.set_caption("First Game")
+
+
+walkleft =[pygame.image.load('L1.png'),
+		pygame.image.load('L2.png'),
+		pygame.image.load('L3.png'),
+		pygame.image.load('L4.png'),
+		pygame.image.load('L5.png'),
+		pygame.image.load('L6.png'),
+		pygame.image.load('L7.png'),
+		pygame.image.load('L8.png'),
+		pygame.image.load('L9.png'),
+		]
+
+walkRight =[pygame.image.load('R1.png'),
+		pygame.image.load('R2.png'),
+		pygame.image.load('R3.png'),
+		pygame.image.load('R4.png'),
+		pygame.image.load('R5.png'),
+		pygame.image.load('R6.png'),
+		pygame.image.load('R7.png'),
+		pygame.image.load('R8.png'),
+		pygame.image.load('R9.png'),
+		]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
+clock = pygame.time.Clock()
+
+x = 50
+y = 400
+width = 64
+height = 64
+vel = 5
+isjump = False
+jumpCount = 10
+left = False
+right = False
+walkCount = 0
+
+def redrawGameWindow():
+	global walkCount
+	win.blit(bg,(0,0))
+
+	if walkcount + 1>= 27:
+		walkcount = 0
+
+
+	if left:
+		win.blit(walkLeft[walkcount//3],(x,y))
+		walkcount += 1
+	elif right:
+		win.blit(walkRight[walkCount//3],(x,y))
+		walkcount += 1
+	else:
+		win.blit(char,(x,y))
+
+	pygame.display.update()
+
+
+run = True
+while run:
+	clock.tick(27)
+
+	for event in pygame.event.get():
+		if event.tupe == pygame.QUIT:
+			run = False
+
+	keys = pygame.key.get_pressed()
+
+	if keys[pygame.K_LEFT] and x > vel:
+		x -= vel
+		left = True
+		right = False
+	elif keys[pygame.K_RIGHT] and x < 500 -width - vel:
+		x += vel
+		right = True
+		left = False
+	else:
+		right = False
+		left = False
+		walkCount = 0
+
+	if not(isJump):
+		if keys[pygame.K_SPACE]:
+			isJump = True
+			right = False
+			left = False
+			walkCount = 0
+
+
+	else:
+		if jumpCount >= -10:
+			neg = 1
+			if jumpCount < 0:
+				neg = -1
+			y -= (jumpCount ==2)*0.5*neg
+			jumpCount-= 1
+		else:
+			isJump = False
+			jumpCount = 10
+
+	redrawGameWindow()
+	pygame.quit()
+
+	https://bit.ly/2UzP
+
--- Sets.py
+++ Sets.py
@@ -1,2 +1,84 @@
 #Sets
 
+Sets.py
+ 
+
+includes a data type for sets.
+Curly braces or the set() function can be used to create sets.
+
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)   # show that duplicates have been removed
+
+'orange' in basket        # fast member testing
+'crabgrass' in basket
+
+Demonstrate set opreations on uniqe letters from
+a = set('abracadabra')
+b = set('alacazm')
+a 						# unique letters in a
+a - b 					# letters in a but not in b
+a | b 					# letters in a or b or both
+a & b 					# letters in both a and b
+a ^ b 					# letters in a or b but not both
+
+
+x = set('23802348')
+y = set('57839012')
+x - y
+y - x
+x ^ y
+y | x
+x & y
+x
+
+a = { x for x in 'abracadabra' if x not in 'abc'}
+abracadabra
+
+--------
+
+friuts = {"apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"}
+print("cherry" in fruits)
+
+friuts.add("cucumber")
+friuts
+friuts.update("grape")
+friuts
+friuts.remove("banana")
+friuts
+friuts.discard("banana")
+friuts
+
+Dictionaries
+
+# Dictionaries
+
+#Another usedul data type built into pyhton is the dictionary
+
+tel = {'jack': 4098,'sape': 4139}
+tel['sape']
+tel['guido'] = 4127
+tel
+
+---------
+
+del tel ['sape']
+tel['irv'] = 4127
+tel
+
+---------
+
+del tel['sape']
+tel [ir]
+
+
+
+
+
+When lopping throgh Dictionaries
+
+knights = {'gallahad': 'the pure','robin': 'the brave','sape' : 4355}
+for k, v in knights.items():
+	print(k, v)
+
+for x, y in enumerate(['tic', 'tac', 'toe']):
+	print(x, y)
